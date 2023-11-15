3e31720ef2719f06b97f5104d6848ae10d2c0fce
Tiago Araujo <tiagomuniz130@gmail.com>
2023-11-15 13:54:10 -0300

    Initial commit

---
+++ i3blocks/sound/sound_info.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+VOLUME_MUTE="󰖁 "
+VOLUME_LOW="󰕿 "
+VOLUME_MID="󰖀 "
+VOLUME_HIGH="󰕾 "
+SOUND_LEVEL=$(pamixer --get-volume --set-limit 100)
+MUTED=$(pamixer --get-mute)
+
+ICON=$VOLUME_MUTE
+if [ "$MUTED" == "true" ]
+then
+    ICON="$VOLUME_MUTE"
+else
+    if [ "$SOUND_LEVEL" -lt 34 ]
+    then
+        ICON="$VOLUME_LOW"
+    elif [ "$SOUND_LEVEL" -lt 67 ]
+    then
+        ICON="$VOLUME_MID"
+    else
+        ICON="$VOLUME_HIGH"
+    fi
+fi
+
+echo "$ICON: $SOUND_LEVEL%" 
+
